0ae141ccbff48cab431485750d5b2893d7bb707d
purwar1997 <shubhampurwar511@gmail.com>
2022-12-23 08:04:54 +0530

    project completed

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -24,14 +24,14 @@ function Form() {
       <h1 className="mt-10 text-3xl text-center">Create new Todo</h1>
 
       <form
-        className="mt-12 mb-16 flex flex-col items-center gap-6"
+        className="mt-10 mb-16 flex flex-col items-center gap-6"
         action=""
         method="post"
         onSubmit={handleSubmit}
       >
         <div className="flex flex-col items-start gap-2">
           <label className="text-lg" htmlFor="title">
-            Enter title for new todo
+            Enter title for a new todo
           </label>
           <input
             className="w-96 bg-[#f6f6f6] border rounded px-4 py-2.5 focus:outline-[#3944F7]"
